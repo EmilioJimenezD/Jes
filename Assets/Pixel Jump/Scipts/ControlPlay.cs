d3a1f195496db614017bee23afafae77220b69ae
EmilioJimenezD <Emilio.jimenez@upb.edu.co>
2023-02-24 01:13:40 -0500

    NoFunciona el OnpressPlay

---
+++ Assets/Pixel Jump/Scipts/ControlPlay.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlay : MonoBehaviour
+{
+public void OnPressPlay() 
+    {
+    Debug.Log("Play")
+    }
+}
