42042a96621277d9870058f1d27c896aed43f0b4
EmilioJimenezD <Emilio.jimenez@upb.edu.co>
2023-04-20 23:23:44 -0500

    Commit, Lo que fue fue

--- Assets/Pixel Jump/Scipts/ControlMuerte.cs
+++ Assets/Pixel Jump/Scipts/ControlMuerte.cs
@@ -2,17 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControlMuerte : MonoBehaviour
+public class ControlMuerte : MonoBehaviour, MMEventListener<CorgiEngineEvent>,MMEventListener<>
+
+[SerializerField]
+
+    private List <GameObject> corazones;
+private interface vidas;
+
+private interface contador_estrellas;
+[SerializedField]
+
+private interface total_estrellas;
+
+public void Start()
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    vidas = corazones.Count;
         
+}
+
+public virtual void OnMMEvent(CorgiEngineEvent e)
+
+    {
+        if(e.EventType == CorgiEngineEventTypes.PlayerDeath)
+    {
+        if(vidas-1>=0)
+        {
+            vidas--;
+            contador_estrellas = 0;
+            corazones[vidas].SetActive(false);
+
+        }
+        if(vidas ==0)
+        {
+            Debug.Log("Game Over");
+        }
+    }
+    Debug.Log(e.EventType);
     }
 
-    // Update is called once per frame
-    void Update()
+public virtual void OnMMEvent(PickableItemEvent e)
+{
+    Debug.Log("Pick");
+    contador_estrellas++;
+    Debug.Log("contador estrellas " + contador_estrellas + " vidas " + vidas);
+    if(contador_estrellas == total_estrellas && vidas+1<=corazones.Count)
     {
-        
+        corazones[vidas].SetActive(true);
+        vidas++;
+        contador_estrellas = object;
     }
 }
+void OnEnable(
+    {
+    this.MMEventStartListening<CorgiEngineEvent>();
+this.MMEventStartListening<PickableItemEvent>();
+}
+
+Void OnDisable()
+{
+    this.MMEventStopListening<CorgiEngineEvent>();
+    this.MMEventStartListening<PickableItemEvent>();
+}
+
