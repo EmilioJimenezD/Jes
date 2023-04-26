09bfd4549f544286718f26319ca89ea8e3ffe9bf
Juanjlopez13 <109627299+Juanjlopez13@users.noreply.github.com>
2023-04-26 09:38:33 -0500

    Scripts para el muro

---
+++ Assets/Pixel Jump/Scipts/EscucharVictoria.cs
@@ -0,0 +1,21 @@
+using MoreMountains.Tools;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+
+public class EscucharVictoria : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject muro;
+
+    public virtual void OnMMEvent(MMGameEvent evento)
+    {
+        Debug.Log("Auxilioo");
+        if (evento.EventName == "victoria")
+        {
+            Debug.Log("Funciona");
+            muro.SetActive(true);
+        }
+    }
+}
