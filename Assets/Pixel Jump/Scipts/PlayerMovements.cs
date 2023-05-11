2747411c3d1c5d206a088ca268877941d7e34561
EmilioJimenezD <Emilio.jimenez@upb.edu.co>
2023-05-11 17:00:30 -0500

    j

--- Assets/Pixel Jump/Scipts/PlayerMovements.cs
+++ Assets/Pixel Jump/Scipts/PlayerMovements.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class PlayerMovements : MonoBehaviour
 {
+    private void FixedUpdate()
+    {
+        Vector3 U
+    }
     // Start is called before the first frame update
     void Start()
     {
